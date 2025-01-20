6bf63a75c94a5f395db16947cc6126140faecb15
mister-critter <cteague1222@gmail.com>
2025-01-19 22:38:16 -0600

    Simplified to show the latest release

--- index.js
+++ index.js
@@ -15,21 +15,9 @@ const myAPIkey = process.env.API_KEY;
 
 app.get("/", async (req, res) => {
     try {
-        const response = await axios.get(`https://comicvine.gamespot.com/api/issues/?api_key=${myAPIkey}&format=json&filter=volume:91078&sort=cover_date:desc`);
+        const response = await axios.get(`https://comicvine.gamespot.com/api/issues/?api_key=${myAPIkey}&format=json&filter=volume:91078&sort=store_date:desc`);
         const result = response.data.results;
-        // Compare today's date to the cover dates in the object,
-        // then pass the issue with the closest cover date
-        let currentIssueIndex;
-        const date = new Date();
-        const todaysDate = date.toISOString().slice(0, 10);
-        for (let i = 0; i < result.length; i++) {
-            if (result[i].cover_date <= todaysDate) {
-                currentIssueIndex = i;
-                break;
-            }
-        }
-
-        res.render("index.ejs", { currentIssue: result[currentIssueIndex], latestIssue: result[0] });
+        res.render("index.ejs", { currentIssue: result[0] });
 
     } catch (error) {
         console.error("Failed to make request:", error.message);
